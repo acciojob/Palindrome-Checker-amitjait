cf7b6742fdfff32b1774d912638048167d1efbe9
Amit Kumar <106420527+amitjait@users.noreply.github.com>
2022-07-11 23:39:36 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -3,9 +3,13 @@
 function palindrome(str){
     let i =0; 
     let j = str.length-1;
-
+    str=str.toLowerCase();
+    
     while(i<=j){
-        if(str.charAt(i++) != str.charAt(j--)){
+        if(str.charAt(i) == str.charAt(j)){
+            i++;
+            j--;
+        }else{
             return false;
         }
     }
