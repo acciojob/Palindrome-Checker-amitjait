365de0cbb8180ea930ca74d02e705766541dce3a
amitjait <amit.rimit591@gmail.com>
2022-07-11 23:29:53 +0530

    updating solution

--- solution.js
+++ solution.js
@@ -1,6 +1,16 @@
 // complete the given function
+const str = document.getElementById('input');
 
 function palindrome(str){
+    let i =0; 
+    let j = str.length-1;
 
+    while(i<=j){
+        if(str.charAt(i++) != str.charAt(j--)){
+            return false;
+        }
+    }
+    return true;
 }
+console.log(palindrome);
 module.exports = palindrome
